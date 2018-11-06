70f94aec87ddc66f8eb6021228067fed9f6a70b8
yueguang <yueguang@qingmutec.com>
2018-11-06 13:54:26 +0800

    Laravel5.5 + Vue2 + Element 整合初始化版本

---
+++ resources/js/router/index.vue
@@ -0,0 +1,18 @@
+<script>
+    import Vue from 'vue';
+    import VueRouter from 'vue-router';
+
+    Vue.use(VueRouter);
+
+    export default new VueRouter({
+        saveScrollPosition: true,
+        routes: [
+            {
+                name: 'hello',
+                path: '/hello',
+                component: resolve => void(require(['../components/Hello.vue'], resolve))
+            }
+        ]
+    });
+</script>
+
