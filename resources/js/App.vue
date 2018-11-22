86eb12a20f958ff40d56340ff9e49a3938fe1d2e
yueguang <yueguang@qingmutec.com>
2018-11-22 14:28:50 +0800

    Laravel5.5 + Vue2 + Element+monogo 整合初始化版本

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Hello, {{ msg }}!</h1>
+        <h1>Hello, {{ msg }}! Monogo 测试环境</h1>
         <router-view></router-view> <!--路由引入的组件将在这里被渲染-->
     </div>
 </template>
@@ -15,5 +15,3 @@
     }
 </script>
 
-<style>
-</style>
